beca3f41a2ea9c05cecfc4d252b8b96a5fc9e03a
GrntKim <grantjun0224@gmail.com>
2026-04-27 14:51:51 +0900

    solve 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(message, num) {
+    if(num<0) return 'ERROR';
+    var s = '';
+    for(var i=0; i<num; i++)
+        s += message;
+    return s;
 };
 
 // Do not edit below this line
