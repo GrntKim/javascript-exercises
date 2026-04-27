e617b9008d493a34700174e4df11087734eee5a6
GrntKim <grantjun0224@gmail.com>
2026-04-27 15:08:24 +0900

    solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(st, ed) {
+    if(st < 0 || ed < 0 || !Number.isInteger(st) || !Number.isInteger(ed)) return 'ERROR';
+    if(st > ed) {
+        var tmp = st;
+        st = ed;
+        ed = tmp;
+    }
+    var ret = 0;
+    for(var i=st; i<=ed; i++) ret += i;
+    return ret;
 };
 
 // Do not edit below this line
