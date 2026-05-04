26dadbbeb9512143d02f868a96623acd14a21ab9
GrntKim <grantjun0224@gmail.com>
2026-05-04 10:16:12 +0900

    solve 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const converted = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0, right = converted.length-1;
+    while(left < right) {
+        if(converted[left] !== converted[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
