abc77074eee8cf51e206f15de90662565b1d8b25
GrntKim <grantjun0224@gmail.com>
2026-05-04 10:28:26 +0900

    solve 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const n = Number(input);
+    if (n < 0) return "OOPS";
+    if (n < 2) return n;
+    let prev = 0, cur = 1;
+    for(let i = 2; i <= n; i++) {
+        [prev, cur] = [cur, prev + cur];
+    }
+    return cur;
 };
 
 // Do not edit below this line
