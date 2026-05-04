214e2a00a87fa12814766f30bef7df1bb3963abf
GrntKim <grantjun0224@gmail.com>
2026-05-04 10:55:57 +0900

    solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const curYear = new Date().getFullYear();
+    const getAge = (p) => (p.yearOfDeath ?? curYear) - p.yearOfBirth;
+    return people.reduce((oldest, person) => {
+        return getAge(oldest) > getAge(person) ? oldest : person;
+    });
 };
 
 // Do not edit below this line
